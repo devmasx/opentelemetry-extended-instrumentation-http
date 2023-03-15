7a0769632a81b010a596a2fe8f7a744b393eef10
Miguel Savignano <migue.masx@gmail.com>
2023-03-15 01:32:57 +0100

    rename module

--- index.spec.ts
+++ index.spec.ts
@@ -1,14 +1,14 @@
-import { InstrumentationsConfig } from './index';
+import { HttpInstrumentation } from './index';
 
 const span = {
   setAttribute: jest.fn(),
   setAttributes: jest.fn(),
 };
 
-describe('InstrumentationsConfig', () => {
+describe('HttpInstrumentation', () => {
   it(`#onRequestBody`, () => {
     // @ts-ignore
-    InstrumentationsConfig.onRequestBody(span, '{"example": "example" }');
+    HttpInstrumentation.onRequestBody(span, '{"example": "example" }');
     expect(span.setAttributes).toHaveBeenLastCalledWith({
       'http.request.body': '{"example": "example" }',
     });
@@ -16,7 +16,7 @@ describe('InstrumentationsConfig', () => {
 
   it(`#onResponseBody`, () => {
     // @ts-ignore
-    InstrumentationsConfig.onResponseBody(span, '{"example": "example" }');
+    HttpInstrumentation.onResponseBody(span, '{"example": "example" }');
     expect(span.setAttributes).toHaveBeenLastCalledWith({
       'http.response.body': '{"example": "example" }',
     });
@@ -24,7 +24,7 @@ describe('InstrumentationsConfig', () => {
 
   it(`#onRequestHeaders`, () => {
     // @ts-ignore
-    InstrumentationsConfig.onRequestHeaders(span, { 'x-header': 'example' });
+    HttpInstrumentation.onRequestHeaders(span, { 'x-header': 'example' });
     expect(span.setAttributes).toHaveBeenLastCalledWith({
       'http.request.headers': '{"x-header":"example"}',
     });
@@ -32,7 +32,7 @@ describe('InstrumentationsConfig', () => {
 
   it(`#onResponseHeaders`, () => {
     // @ts-ignore
-    InstrumentationsConfig.onResponseHeaders(span, { 'x-header': 'example' });
+    HttpInstrumentation.onResponseHeaders(span, { 'x-header': 'example' });
     expect(span.setAttributes).toHaveBeenLastCalledWith({
       'http.response.headers': '{"x-header":"example"}',
     });
